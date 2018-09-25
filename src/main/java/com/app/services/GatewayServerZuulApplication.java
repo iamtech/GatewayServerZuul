c63628f79f62de15219c27201f50ae3b2cce1053
Amit Kumar Singh <amit.singh@tavant.com>
2018-09-25 17:29:59 +0530

    Added routing for service --> employee-microservice-dataset

---
+++ src/main/java/com/app/services/GatewayServerZuulApplication.java
@@ -0,0 +1,37 @@
+package com.app.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.app.services.filters.ErrorFilter;
+import com.app.services.filters.PostFilter;
+import com.app.services.filters.PreFilter;
+import com.app.services.filters.RouteFilter;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayServerZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerZuulApplication.class, args);
+	}
+	
+	@Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+}
