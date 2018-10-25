c87d36c9957f53c31fc61aa1f29ccab589ce133d
Amit Kumar Singh <amit.singh@tavant.com>
2018-10-25 13:01:58 +0530

    added basic JWT authorization to access service

--- src/main/java/com/app/services/GatewayServerZuulApplication.java
+++ src/main/java/com/app/services/GatewayServerZuulApplication.java
@@ -2,6 +2,7 @@ package com.app.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -12,6 +13,7 @@ import com.app.services.filters.RouteFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerZuulApplication {
 
 	public static void main(String[] args) {
